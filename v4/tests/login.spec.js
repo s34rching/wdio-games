cec311ad1fac0b834d830ebfe0a1ef142b314102
s34rching <ivan.katsenya@gmail.com>
2021-01-30 21:20:37 +0300

    Basic setup

---
+++ v4/tests/login.spec.js
@@ -0,0 +1,13 @@
+const webdriverio = require('webdriverio');
+const options = { desiredCapabilities: { browserName: 'chrome' } };
+const client = webdriverio.remote(options);
+
+client
+  .init()
+  .url('http://www.webdriveruniversity.com/')
+  .click('#login-portal')
+  .getTitle()
+  .then(function(title) {
+    console.log('Title is: ' + title);
+  })
+  .end();
