d844f0bdef5704ac90d8f4ff885a08fde2addca4
s34rching <ivan.katsenya@gmail.com>
2021-02-04 20:27:54 +0300

    To async / await

--- v4/tests/webdriver-university.spec.js
+++ v4/tests/webdriver-university.spec.js
@@ -1,22 +1,13 @@
 describe("Verify whether webdriveruniversity links on homepage work correctly", function() {
-    it("should open 'Contact Us' page", function() {
-        return browser
-            .setViewportSize({ width: 1200, height: 800 })
-            .url('http://www.webdriveruniversity.com/')
-            .getTitle().then(function(title) {
-                console.log('Title is: ' + title);
-            })
-            .click("#contact-us")
-            .pause(3000)
+    it("should open 'Contact Us' page", async function() {
+        await browser.setViewportSize({ width: 1200, height: 800 })
+        await browser.url('http://www.webdriveruniversity.com/')
+        await browser.click("#contact-us")
+        await browser.pause(3000)
     });
 
-    it("should open 'Login Portal'", function() {
-        return browser
-            .url('http://www.webdriveruniversity.com/')
-            .click('#login-portal')
-            .getTitle()
-            .then(function(title) {
-                console.log('Title is: ' + title);
-            });
+    it("should open 'Login Portal'", async function() {
+        await browser.url('http://www.webdriveruniversity.com/')
+        await browser.click('#login-portal')
     });
 });
