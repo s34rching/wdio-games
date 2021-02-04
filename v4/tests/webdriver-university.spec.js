8dd832f540d1ade53fecee6ac151b99a15ed8485
s34rching <ivan.katsenya@gmail.com>
2021-02-04 20:59:12 +0300

    Update 'baseUrl'

--- v4/tests/webdriver-university.spec.js
+++ v4/tests/webdriver-university.spec.js
@@ -1,13 +1,13 @@
 describe("Verify whether webdriveruniversity links on homepage work correctly", function() {
     it("should open 'Contact Us' page", async function() {
         await browser.setViewportSize({ width: 1200, height: 800 })
-        await browser.url('http://www.webdriveruniversity.com/')
+        await browser.url('/')
         await browser.click("#contact-us")
         await browser.pause(3000)
     });
 
     it("should open 'Login Portal'", async function() {
-        await browser.url('http://www.webdriveruniversity.com/')
+        await browser.url('/')
         await browser.click('#login-portal')
         await browser.pause(3000)
     });
