b6486ffe33861c746be869bd550c909439fade0a
s34rching <ivan.katsenya@gmail.com>
2021-02-06 10:10:21 +0300

    Switch between tabs

--- v4/tests/homepage.spec.js
+++ v4/tests/homepage.spec.js
@@ -3,22 +3,24 @@ describe("Verify whether webdriveruniversity links on homepage work correctly", 
         await browser.setViewportSize({ width: 1200, height: 800 });
         await browser.url('/');
         await browser.click("#contact-us");
-        const title = await browser.getTitle();
-        expect(title).to.equal('WebDriverUniversity.com')
-        await browser.pause(3000);
-    });
 
-    it("link to 'Contact Us' page should have proper color", async function() {
-        await browser.url('/');
-        const linkColor = await browser.getCssProperty("#contact-us", 'color');
-        expect(linkColor.value).to.equal('rgba(51,122,183,1)')
+        const tabIds = await browser.getTabIds();
+        await browser.switchTab(tabIds[1])
+
+        const title = await browser.getTitle();
+        title.should.equal('WebDriver | Contact Us')
+        await browser.close()
     });
 
     it("should open 'Login Portal'", async function() {
         await browser.url('/');
         await browser.click('#login-portal');
+
+        const tabIds = await browser.getTabIds();
+        await browser.switchTab(tabIds[1])
+
         const title = await browser.getTitle();
-        title.should.equal('WebDriverUniversity.com')
-        await browser.pause(3000);
+        title.should.equal('WebDriver | Login Portal')
+        await browser.close()
     });
 });
