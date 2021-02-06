383af95a3adec932091058b0520d0e573d892099
s34rching <ivan.katsenya@gmail.com>
2021-02-06 17:59:29 +0300

    Add assertions to 'Contact Us' form tests

--- v4/tests/contact-us.spec.js
+++ v4/tests/contact-us.spec.js
@@ -9,7 +9,9 @@ describe('"Contact Us"', function() {
         await browser.setValue('[name="email"]', 'john_doe@domain.com')
         await browser.setValue('[name="message"]', 'The most exiting thing!')
         await browser.click('[type="submit"]')
-        await browser.pause(3000)
+
+        const reply = await browser.getText('#contact_reply h1');
+        expect(reply).to.equal('Thank You for your Message!');
     });
 
     it('should NOT be able to submit a form with missing first name', async function() {
@@ -19,6 +21,9 @@ describe('"Contact Us"', function() {
         await browser.setValue('[name="message"]', 'The most exiting thing!')
         await browser.click('[type="submit"]')
         await browser.pause(3000)
+
+        const isExisting = await browser.isExisting('#contact_reply h1');
+        expect(isExisting).to.be.false;
     });
 
     it('should NOT be able to submit a form with missing last name', async function() {
@@ -27,7 +32,9 @@ describe('"Contact Us"', function() {
         await browser.setValue('[name="email"]', 'john_doe@domain.com')
         await browser.setValue('[name="message"]', 'The most exiting thing!')
         await browser.click('[type="submit"]')
-        await browser.pause(3000)
+
+        const isExisting = await browser.isExisting('#contact_reply h1');
+        expect(isExisting).to.be.false;
     });
 
     it('should NOT be able to submit a form with missing email', async function() {
@@ -36,6 +43,8 @@ describe('"Contact Us"', function() {
         await browser.setValue('[name="email"]', '')
         await browser.setValue('[name="message"]', 'The most exiting thing!')
         await browser.click('[type="submit"]')
-        await browser.pause(3000)
+
+        const errorText = await browser.getText('body');
+        expect(errorText).to.include('Error: all fields are required');
     });
 });
