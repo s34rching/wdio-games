5946171458460001e04e4042edb14b741b4d63c6
s34rching <ivan.katsenya@gmail.com>
2021-02-06 09:55:08 +0300

    hooks!

--- v4/tests/contact-us.spec.js
+++ v4/tests/contact-us.spec.js
@@ -4,21 +4,38 @@ describe('"Contact Us"', function() {
     })
 
     it('should be able to submit a successful submission via contact us form', async function() {
-        await browser.setValue('[name="first_name"]', 'ads')
-        await browser.setValue('[name="last_name"]', 'ads')
-        await browser.setValue('[name="email"]', 'ads')
-        await browser.setValue('[name="message"]', 'ads')
+        await browser.setValue('[name="first_name"]', 'John')
+        await browser.setValue('[name="last_name"]', 'Doe')
+        await browser.setValue('[name="email"]', 'john_doe@domain.com')
+        await browser.setValue('[name="message"]', 'The most exiting thing!')
+        await browser.click('[type="submit"]')
+        await browser.pause(3000)
     });
 
-    it('should NOT be able to submit a successful submission via contact us form as all fields are required', async function() {
-
+    it('should NOT be able to submit a form with missing first name', async function() {
+        await browser.setValue('[name="first_name"]', '')
+        await browser.setValue('[name="last_name"]', 'Doe')
+        await browser.setValue('[name="email"]', 'john_doe@domain.com')
+        await browser.setValue('[name="message"]', 'The most exiting thing!')
+        await browser.click('[type="submit"]')
+        await browser.pause(3000)
     });
 
-    it('should NOT be able to submit a successful submission via contact us form as all fields are required', async function() {
-
+    it('should NOT be able to submit a form with missing last name', async function() {
+        await browser.setValue('[name="first_name"]', 'John')
+        await browser.setValue('[name="last_name"]', '')
+        await browser.setValue('[name="email"]', 'john_doe@domain.com')
+        await browser.setValue('[name="message"]', 'The most exiting thing!')
+        await browser.click('[type="submit"]')
+        await browser.pause(3000)
     });
 
-    it('should NOT be able to submit a successful submission via contact us form as all fields are required', async function() {
-
+    it('should NOT be able to submit a form with missing email', async function() {
+        await browser.setValue('[name="first_name"]', 'John')
+        await browser.setValue('[name="last_name"]', 'Doe')
+        await browser.setValue('[name="email"]', '')
+        await browser.setValue('[name="message"]', 'The most exiting thing!')
+        await browser.click('[type="submit"]')
+        await browser.pause(3000)
     });
 });
