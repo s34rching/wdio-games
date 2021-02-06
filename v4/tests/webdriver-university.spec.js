da00be801cec9b3f5cb590160f82f8247aa6aadf
s34rching <ivan.katsenya@gmail.com>
2021-02-06 09:05:21 +0300

    'getCssProperty' in action

--- v4/tests/webdriver-university.spec.js
+++ v4/tests/webdriver-university.spec.js
@@ -8,6 +8,12 @@ describe("Verify whether webdriveruniversity links on homepage work correctly", 
         await browser.pause(3000);
     });
 
+    it("link to 'Contact Us' page should have proper color", async function() {
+        await browser.url('/');
+        const linkColor = await browser.getCssProperty("#contact-us", 'color');
+        expect(linkColor.value).to.equal('rgba(51,122,183,1)')
+    });
+
     it("should open 'Login Portal'", async function() {
         await browser.url('/');
         await browser.click('#login-portal');
