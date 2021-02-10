9f6b9f7a9dd0eb8ac424032eeec63b6580d05d47
s34rching <ivan.katsenya@gmail.com>
2021-02-10 16:08:19 +0300

    Add custom command

--- v4-async/tests/contact-us.spec.js
+++ v4-async/tests/contact-us.spec.js
@@ -1,3 +1,19 @@
+browser.addCommand("sendMessageToCompany", async function({ firstName, lastName, email, message }) {
+    if (firstName) {
+        await browser.setValue('[name="first_name"]', firstName)
+    }
+    if (lastName) {
+        await browser.setValue('[name="last_name"]', lastName)
+    }
+    if (email) {
+        await browser.setValue('[name="email"]', email)
+    }
+    if (message) {
+        await browser.setValue('[name="message"]', message)
+    }
+    await browser.click('[type="submit"]')
+})
+
 describe('"Contact Us"', function() {
     beforeEach(async function() {
         await browser.url('/Contact-Us/contactus.html');
@@ -5,11 +21,12 @@ describe('"Contact Us"', function() {
 
     usersData.forEach((user) => {
         it('should be able to submit a successful submission via contact us form', async function() {
-            await browser.setValue('[name="first_name"]', 'John')
-            await browser.setValue('[name="last_name"]', 'Doe')
-            await browser.setValue('[name="email"]', user.email)
-            await browser.setValue('[name="message"]', user.message)
-            await browser.click('[type="submit"]')
+            await browser.sendMessageToCompany({
+                firstName: 'John',
+                lastName: 'Doe',
+                email: user.email,
+                message: user.message
+            });
 
             const reply = await browser.getText('#contact_reply h1');
             expect(reply).to.equal('Thank You for your Message!');
@@ -17,34 +34,33 @@ describe('"Contact Us"', function() {
     })
 
     it('should NOT be able to submit a form with missing first name', async function() {
-        await browser.setValue('[name="first_name"]', '')
-        await browser.setValue('[name="last_name"]', 'Doe')
-        await browser.setValue('[name="email"]', 'john_doe@domain.com')
-        await browser.setValue('[name="message"]', 'The most exiting thing!')
-        await browser.click('[type="submit"]')
-        await browser.pause(3000)
+        await browser.sendMessageToCompany({
+            lastName: 'Doe',
+            email: 'john_doe@domain.com',
+            message: 'The most exiting thing!',
+        });
 
         const isExisting = await browser.isExisting('#contact_reply h1');
         expect(isExisting).to.be.false;
     });
 
     it('should NOT be able to submit a form with missing last name', async function() {
-        await browser.setValue('[name="first_name"]', 'John')
-        await browser.setValue('[name="last_name"]', '')
-        await browser.setValue('[name="email"]', 'john_doe@domain.com')
-        await browser.setValue('[name="message"]', 'The most exiting thing!')
-        await browser.click('[type="submit"]')
+        await browser.sendMessageToCompany({
+            firstName: 'John',
+            email: 'john_doe@domain.com',
+            message: 'The most exiting thing!',
+        });
 
         const isExisting = await browser.isExisting('#contact_reply h1');
         expect(isExisting).to.be.false;
     });
 
     it('should NOT be able to submit a form with missing email', async function() {
-        await browser.setValue('[name="first_name"]', 'John')
-        await browser.setValue('[name="last_name"]', 'Doe')
-        await browser.setValue('[name="email"]', '')
-        await browser.setValue('[name="message"]', 'The most exiting thing!')
-        await browser.click('[type="submit"]')
+        await browser.sendMessageToCompany({
+            firstName: 'John',
+            lastName: 'Doe',
+            message: 'The most exiting thing!',
+        });
 
         const errorText = await browser.getText('body');
         expect(errorText).to.include('Error: all fields are required');
