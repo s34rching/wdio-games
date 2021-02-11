3c823313e3f2f0c4ec2e97c6caa033175b9fd89e
s34rching <ivan.katsenya@gmail.com>
2021-02-11 11:47:42 +0300

    Add 'Allure' reporter

--- v4-async/wdio.conf.js
+++ v4-async/wdio.conf.js
@@ -134,7 +134,13 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/reporters/dot.html
-    // reporters: ['dot'],
+    reporters: ['dot', 'allure'],
+
+    reporterOptions: {
+        allure: {
+            outputDir: './reports/'
+        },
+    },
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
