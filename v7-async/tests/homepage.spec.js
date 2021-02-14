9ea081fe990fe52cdb31096317cdb81a3274d9b0
s34rching <ivan.katsenya@gmail.com>
2021-02-14 23:10:12 +0300

    v7 start

---
+++ v7-async/tests/homepage.spec.js
@@ -0,0 +1,26 @@
+describe("Verify whether webdriveruniversity links on homepage work correctly", function() {
+    it("should open 'Contact Us' page", async function() {
+        await browser.setViewportSize({ width: 1200, height: 800 });
+        await browser.url('/');
+        await browser.click("#contact-us");
+
+        const tabIds = await browser.getTabIds();
+        await browser.switchTab(tabIds[1])
+
+        const title = await browser.getTitle();
+        title.should.equal('WebDriver | Contact Us')
+        await browser.close()
+    });
+
+    it("should open 'Login Portal'", async function() {
+        await browser.url('/');
+        await browser.click('#login-portal');
+
+        const tabIds = await browser.getTabIds();
+        await browser.switchTab(tabIds[1])
+
+        const title = await browser.getTitle();
+        title.should.equal('WebDriver | Login Portal')
+        await browser.close()
+    });
+});
