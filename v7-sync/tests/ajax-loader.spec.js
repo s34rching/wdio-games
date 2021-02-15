d5c8a842d315d41832a5da0e1a5cf7263ab401d0
s34rching <ivan.katsenya@gmail.com>
2021-02-15 12:34:39 +0300

    Force clicking

--- v7-sync/tests/ajax-loader.spec.js
+++ v7-sync/tests/ajax-loader.spec.js
@@ -1,10 +1,25 @@
 import AjaxLoaderPage from '../page-objects/ajax-loader.page';
 
 describe('Ajax Loader Page', () => {
+    beforeEach(() => {
+        browser.addCommand('forceClick', function() {
+            browser.execute(function(element) {
+                element.click();
+            }, this);
+        }, true)
+    })
+
+
     it('should call the modal', () => {
         AjaxLoaderPage.open('/Ajax-Loader/index.html');
         AjaxLoaderPage.clickMeButton.waitForDisplayed();
         AjaxLoaderPage.clickMeButton.click();
+        expect(AjaxLoaderPage.notificationModal).toHaveTextContaining('The waiting game can be a tricky one');
+    })
+
+    it('should call the modal / ^_^', () => {
+        AjaxLoaderPage.open('/Ajax-Loader/index.html');
+        AjaxLoaderPage.clickMeButton.forceClick();
         expect(AjaxLoaderPage.notificationModal).toHaveTextContaining('The waiting game can be a tricky one');
     })
 })
